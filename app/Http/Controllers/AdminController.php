c497f14fa6a53b9b6cd6b76620302531be8e7620
James <noahjamesobekpa101@gmail.com>
2024-02-26 16:27:00 -0800

    commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    
+}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 
+use App\Models\User;
 
 use Illuminate\Http\Request;
 
-use App\Models\User;
+
 
 class HomeController extends Controller
 {
